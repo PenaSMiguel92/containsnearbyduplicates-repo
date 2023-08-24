7eee7fb8041f631cdfdbc96721b6b07e49270578
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-23 21:11:06 -0700

    main implemented

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Solution sol = new Solution();
+        System.out.println(sol.containsNearbyDuplicate(new int[] { 1, 2, 3, 1 }, 3));
+        System.out.println(sol.containsNearbyDuplicate(new int[] { 1, 0, 1, 1 }, 1));
+        System.out.println(sol.containsNearbyDuplicate(new int[] {1, 2, 3, 1, 2, 3}, 2));
     } 
 } 
